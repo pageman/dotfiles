30774e41d34ffa31820f5360af1386aa3acd48da
Joel McCracken <mccracken.joel@gmail.com>
2013-06-01 13:15:59 -0400

    add lib

---
+++ lib/system-servers.el
@@ -0,0 +1,14 @@
+(message "System servers are loaded")
+
+(message "Starting ruby notifications service")
+
+(progn
+  (shell "ruby-system-service")
+  (with-current-buffer "ruby-system-service"
+    (insert "~/bin/system-server")
+    (comint-send-input))
+
+  (progn 
+    (shell "personal-emacs")
+    )
+  )
