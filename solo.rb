f34a7c788e893d7df8b77184a1226e1a140eab75
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-06-27 20:04:57 -0400

    removes personal cookbook from librarian-chef
    this removes the need for me to run librarian-chef soooo much.

--- solo.rb
+++ solo.rb
@@ -1,4 +1,5 @@
 
 current_dir = File.expand_path(File.dirname __FILE__)
 file_cache_path current_dir
-cookbook_path File.join current_dir, "cookbooks"
+cookbook_path [File.join(current_dir, "cookbooks"),
+               File.join(current_dir, "site-cookbooks")]
