62815bbe93a7e19c0c34637dd3712d3101371e97
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-07-05 10:46:32 -0400

    Adds parameter-passing to chef-solo in run-chef.sh
    This makes it possible to modify the way chef-solo runs; e.g. use
    --why-run or set log level verbosity, even use --help to see the options.

---
+++ bin/run-chef.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# include reset environment code
+# necessary for the chef-version of ruby not to
+# be confused about locations of files,
+# at least on my system. YMMV
+unset GEM_HOME
+unset GEM_PATH
+
+# make sure to put the omnibus-installed version of chef at
+# the front of the path
+PATH="/opt/chef/bin:/opt/chef/embedded/bin:$PATH"
+
+# absolute paths to executables
+# are used to avoid problems with RVM.
+sudo /opt/chef/bin/chef-solo -c solo.rb -j solo.json "$@"
