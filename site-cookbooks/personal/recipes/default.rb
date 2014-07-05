833ac75c44c2ac2e89bbe37824f5c8b5bc62da84
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-07-05 12:31:43 -0400

    adds Firefox profile definition to personal cookb
    Besides Emacs, Firefox is the tool that I rely most heavily upon. I want
    to automate its installation if at all possible.

--- site-cookbooks/personal/recipes/default.rb
+++ site-cookbooks/personal/recipes/default.rb
@@ -51,3 +51,35 @@ shadow_directory "Downloads -> Inbox" do
   replace File.expand_path("~/Downloads")
   with    File.expand_path("~/Inbox")
 end
+
+expand_file = ->(name){
+  ::File.expand_path ::File.join(__FILE__, "../../files/default", name)
+}
+
+hashed_pw = ::File.read(::File.expand_path "~/var/secrets/fx-last-pass-pw-hash")
+lastpass_encoded_pw = %Q{user_pref("extensions.lastpass.loginpws", "mccracken.joel%40gmail.com=#{hashed_pw}");}
+
+personal_firefox_profile "Personal" do
+  owner node[:username]
+  group "staff"
+
+  location File.expand_path("~/var/FirefoxProfiles/Personal")
+
+  extensions ["mozrepl-1.1.2-fx.xpi",
+              "firebug-addon-1843-latest.xpi",
+              "lastpass-addon-8542-latest.xpi",
+              "pinboard.xpi",
+              "pocket.xpi"
+             ].map &expand_file
+
+  prefs ['user_pref("extensions.mozrepl.autoStart", true);',
+
+         #lastpass
+         'user_pref("extensions.lastpass.ffhasloggedinsuccessfully", true);',
+         'user_pref("extensions.lastpass.rememberPassword", true);',
+         'user_pref("extensions.lastpass.rememberUsername", true);',
+         lastpass_encoded_pw,
+         '"user_pref("extensions.lastpass.loginusers", "mccracken.joel%40gmail.com")',
+        ]
+
+end
