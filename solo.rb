363498df19ccb1003a38daa40de22a489cf68fbb
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-07-21 01:17:32 -0400

    add data bag dir

--- solo.rb
+++ solo.rb
@@ -3,3 +3,4 @@ current_dir = File.expand_path(File.dirname __FILE__)
 file_cache_path ::File.join(current_dir, "tmp", "cache")
 cookbook_path [File.join(current_dir, "cookbooks"),
                File.join(current_dir, "site-cookbooks")]
+data_bag_path ::File.join(current_dir, "data_bags")
