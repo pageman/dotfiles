7df3e5cd2808e52ba09fe6e7e6eb82c401120a26
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-07-26 15:40:36 -0400

    install xcode tools

--- site-cookbooks/personal/recipes/install_xcode.rb
+++ site-cookbooks/personal/recipes/install_xcode.rb
@@ -3,10 +3,13 @@
 # install xcode
 #
 
-secret = SecretSource.autofind
-xcode_url = Chef::EncryptedDataBagItem.load("default", "default", secret)["xcode_url"]
+# this version of xcode tools is for mavericks
+if node[:platform] == "mac_os_x" && node[:platform_version] =~ /10\.9\.\d+/
+  secret = SecretSource.autofind
+  xcode_url = Chef::EncryptedDataBagItem.load("default", "default", secret)["xcode_url"]
 
-dmg_package "XCode Tools" do
-  source xcode_url
-  action :install
+  dmg_package "XCode Tools" do
+    source xcode_url
+    action :install
+  end
 end
