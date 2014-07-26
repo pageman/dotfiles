1458748bd01c68fca5e318ed03d560240b30a056
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-07-26 09:04:05 -0400

    fixes location of -R in chown

--- bin/install-chef-standalone.sh
+++ bin/install-chef-standalone.sh
@@ -3,7 +3,7 @@
 curl -LO https://www.opscode.com/chef/install.sh
 sudo bash install.sh
 rm -rf install.sh
-sudo bash -c 'chown $SUDO_USER:staff -R /opt/chef/*'
+sudo bash -c 'chown -R $SUDO_USER:staff /opt/chef/*'
 /opt/chef/embedded/bin/gem install librarian-chef
 /opt/chef/embedded/bin/gem install knife-solo
 /opt/chef/embedded/bin/gem install knife-solo_data_bag
