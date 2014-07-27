61db85640aa2705596295e5ec35ac24a35a9455d
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-07-27 13:39:49 -0400

    convert dotfiles dir into git repo

--- site-cookbooks/bootstrap/recipes/default.rb
+++ site-cookbooks/bootstrap/recipes/default.rb
@@ -18,3 +18,22 @@ if node[:platform] == "mac_os_x" && node[:platform_version] =~ /10\.9\.\d+/
     app "Command Line Tools (OS X 10.9)"
   end
 end
+
+# assume we now have git
+# adapted from
+# http://stackoverflow.com/questions/2411031/how-do-i-clone-into-a-non-empty-directory
+
+dotfiles = ::File.expand_path("~/dotfiles/")
+dotfiles_git = ::File.join(dotfiles, ".git")
+bash "make the dotfiles directory become a git repository" do
+  cwd dotfiles
+  user node[:current_user]
+  not_if { ::Dir.exist? dotfiles_git }
+  code <<-EOC
+    git clone --no-checkout https://github.com/joelmccracken/dotfiles.git dotfiles-tmp
+    mv dotfiles-tmp/.git #{dotfiles_git}
+    rmdir dotfiles-tmp
+    cd #{dotfiles_git}/..
+    git reset --hard HEAD
+  EOC
+end
