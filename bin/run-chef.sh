c2d81797c96ed6f6a83d956aa2eec40bbb4659cc
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-07-26 21:59:18 -0400

    attempt to fix ownership

--- bin/run-chef.sh
+++ bin/run-chef.sh
@@ -16,5 +16,7 @@ if [[ "$EDB_SECRET" != "" && -f "$EDB_SECRET" ]]; then
 fi
 
 sudo bash -c "GIT_SSL_NO_VERIFY=true /opt/chef/bin/chef-solo -c solo.rb -j bootstrap.json $@"
-/opt/chef/embedded/bin/librarian-chef install --verbose
+
+sudo /opt/chef/embedded/bin/librarian-chef install --verbose
 sudo bash -c "GIT_SSL_NO_VERIFY=true /opt/chef/bin/chef-solo -c solo.rb -j solo.json $@"
+sudo bash -c 'chown -R $SUDO_USER:staff ./*'
