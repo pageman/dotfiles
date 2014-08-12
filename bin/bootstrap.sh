de67e61ef180912e363fb9068dc56aa5bd03520f
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-08-12 18:47:57 -0400

    Switch to using omnibus-env script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -6,5 +6,5 @@ if [[ "$EDB_SECRET" != "" && -f "$EDB_SECRET" ]]; then
   ln -s "$EDB_SECRET" ./encrypted_data_bag_secret
 fi
 
-/opt/chef/bin/chef-solo -c solo.rb -j solo.json $@
+/opt/chef/bin/chef-solo -c solo.rb -j bootstrap.json $@
 chown -R $SUDO_USER:staff ./*
