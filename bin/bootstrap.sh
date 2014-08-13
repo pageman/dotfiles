bc5d7714c6d33be57e1aa519ae9718b7fd6e84dc
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-08-12 20:14:19 -0400

    Escape when ln fails
    since I added set -e, ln wont silently fail if the target already
    exists, like I want it to.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$EDB_SECRET" != "" && -f "$EDB_SECRET" ]]; then
-  ln -s "$EDB_SECRET" ./encrypted_data_bag_secret
+  ln -s "$EDB_SECRET" ./encrypted_data_bag_secret || true
 fi
 
 /opt/chef/bin/chef-solo -c solo.rb -j bootstrap.json $@
