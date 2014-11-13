d9e0b84f5eb01fc8b66fd42b75062047b72d9617
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-11-13 01:46:14 -0500

    split sprout recipes into separate chef run
    this way all those dependencies/systems are totally separate. This way,
    I don't need to worry about their explicitly-tested-for sudo requirement.

--- bin/run-chef.sh
+++ bin/run-chef.sh
@@ -6,5 +6,6 @@ if [[ "$EDB_SECRET" != "" && -f "$EDB_SECRET" ]]; then
   ln -s "$EDB_SECRET" ./encrypted_data_bag_secret || true
 fi
 
+sudo /opt/chef/bin/chef-solo -c solo.rb -j sprout.json $@
 /opt/chef/bin/chef-solo -c solo.rb -j solo.json $@
 chown -R $SUDO_USER:staff ./*
