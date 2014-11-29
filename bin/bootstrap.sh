86158ad0566e8b7a8cb13628ce4d99819a3664f1
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-11-29 01:25:56 -0500

    move gem installation around
    I suspect that an ordering issue is the root cause of the current
    wierdness. Basically, the bootstrap script (which installs xcode)
    needs to run before any gem installs can run, because these gems have c
    extensions

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -9,4 +9,7 @@ fi
 /opt/chef/bin/chef-solo -c solo.rb -j bootstrap.json $@
 chown -R $SUDO_USER:staff ./*
 
+/opt/chef/embedded/bin/gem install librarian-chef
+/opt/chef/embedded/bin/gem install knife-solo
+/opt/chef/embedded/bin/gem install knife-solo_data_bag
 /opt/chef/embedded/bin/librarian-chef install --verbose
--- bin/install-chef-standalone.sh
+++ bin/install-chef-standalone.sh
@@ -15,6 +15,3 @@ curl -LO https://www.opscode.com/chef/install.sh
 sudo_fn bash install.sh
 rm -rf install.sh
 sudo_fn bash -c 'chown -R $SUDO_USER:staff /opt/chef/*'
-/opt/chef/embedded/bin/gem install librarian-chef
-/opt/chef/embedded/bin/gem install knife-solo
-/opt/chef/embedded/bin/gem install knife-solo_data_bag
