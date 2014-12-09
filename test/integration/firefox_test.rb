3e0a5ec3091f7ff945a37d78e9780adad8533f25
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-12-09 00:03:53 -0500

    build firefox/mozrepl test

---
+++ test/integration/firefox_test.rb
@@ -0,0 +1,21 @@
+require_relative './test_helper'
+
+describe "firefox" do
+  it "runs firefox & can connect to mozrepl" do
+    pid = spawn(*%W{/Applications/Firefox.app/Contents/MacOS/firefox -P Personal})
+    sleep 5
+    begin
+      require 'socket'
+      tcp = TCPSocket.new 'localhost', 4243
+      read_ready, write_ready = IO.select([tcp], [], [], 5)
+      read_ready.first.gets # move past newline
+      read_ready.first.gets.must_match /MozRepl/
+    ensure
+      Process.kill "KILL", pid
+    end
+  end
+end
+
+def dir_exists? dir
+  Dir.exists? File.expand_path(dir)
+end
--- test/integration/test_helper.rb
+++ test/integration/test_helper.rb
@@ -1,4 +1,4 @@
-
-
-
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
 
