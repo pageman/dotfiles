940fb309ed718a1b408af833b89006c95e82525a
Joel McCracken <jmccracken@thinkthroughmath.com>
2014-12-26 21:56:11 -0500

    [backport] formatting changes

--- site-cookbooks/bootstrap/libraries/secret_source.rb
+++ site-cookbooks/bootstrap/libraries/secret_source.rb
@@ -10,10 +10,9 @@ class SecretSource
     @secret_file_location ||=
       begin
         possible_locations =
-          [
-           "~/var/secrets/encrypted_data_bag_secret",
+          ["~/var/secrets/encrypted_data_bag_secret",
            ::File.join(::File.dirname(__FILE__), '../../../', 'encrypted_data_bag_secret'),
-          ].map {|file| ::File.expand_path(file) }
+         ].map {|file| ::File.expand_path(file) }
         found = possible_locations.find { |file| ::File.exist? file }
 
         unless found
